5ffef676029cc31ab676e35aae29e20afeea6b16
ErickJoestar <erickborquez@gmail.com>
2023-02-15 18:41:52 -0600

    add SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { GetServerSideProps } from 'next';
+
+interface Props {
+  mockData: string;
+}
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const { req, res } = context;
+  console.log(req, res);
+  return { props: { mockData: 'mock data' } };
+};
+  
+
+export default function Home({ mockData }:Props) {
   return (
     <main>
       <h1>Firebase Next SSR Auth</h1>
+      <p>Server side data: { mockData }</p>
     </main>
   )
 }
