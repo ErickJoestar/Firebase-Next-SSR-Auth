e70645f88c4ee7b884233174d8f908324f66a158
ErickJoestar <erickborquez@gmail.com>
2023-02-15 20:30:02 -0600

    add non static page

---
+++ src/pages/[id].tsx
@@ -0,0 +1,8 @@
+export default function NonStaticPage() {
+  return (
+    <main>
+      <h1>Non Static Page</h1>
+      <p>When this page exists, it ss detected and the CF is created</p>
+    </main>
+  )
+}
